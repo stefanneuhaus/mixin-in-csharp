b3ab001315942a77455ee6095b3c6722143bc41f
Stefan Neuhaus <stefan.neuhaus@rewe-digital.com>
2018-04-11 19:01:59 +0200

    Multiple inheritence --- does not compile

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Singer
+{
+    public void Sing()
+    {
+        Console.WriteLine("la la LAAA ...");
+    }
+}
+
+class Drinker
+{
+    public void Drink()
+    {
+        Console.WriteLine("*sip*");
+    }
+}
+
+/*
+ * !! no valid C# code --- does not compile !!
+ *
+ * error CS1721: Class 'Bird' cannot have multiple base classes: 'Singer' and 'Drinker'
+ */
+class Bird : Singer, Drinker { }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bird bird = new Bird();
+        bird.Sing();
+        bird.Drink();
+    }
+}
